bbbd1a49c9af8eb92935ec00f511175120da64cc
Mohit Dhiman <dhimanmohit2548@seasiainfotech.com>
2021-03-12 13:38:49 +0530

    added changes

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  // const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = 'Hyla';
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`${github.actor} has pushed a change to  ${ github.repository }.`)
+  console.log(`This code was committed by: ${ github.actor }`)
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
